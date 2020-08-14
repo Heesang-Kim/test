af03b91ec08ac185f0eee908a0de56fcabaa90d4
김희상 <drhhy1@gmail.com>
2020-08-14 19:23:52 +0900

    test

--- 김서방.py
+++ 김서방.py
@@ -1,11 +1,16 @@
-def solution(seoul):
+'''
+def solution1(seoul):
     x=["kim"]
     d=-1
     for x in seoul:
         d+=1
-        if x
+        if x:
             break
     answer = "김서방은 %d 에 있다" % d
     return answer
+'''
+def solution(seoul):
+    idx = seoul.index("Kim")
+    return idx
 
 print(solution(["Jane", "Kim"]))
