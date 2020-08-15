cf2a5d744ef1dc4c61db6e9c0c9390ef36be1efe
김희상 <drhhy1@gmail.com>
2020-08-15 11:15:35 +0900

    gy

--- for반복제거.py
+++ for반복제거.py
@@ -1,9 +1,9 @@
 def solution(arr):
-    list=[]
+    llist=[]
     for v in arr:
-        if v not in list:
-            list.append(v)
-    answer=list
+        if v not in llist:
+            llist.append(v)
+    answer=llist
     # [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
     print('Hello Python')
     return answer
